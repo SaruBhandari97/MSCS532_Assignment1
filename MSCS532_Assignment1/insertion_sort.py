2c3425153d17b23eb468af2dbdb422faf3f83ad4
sarub <sarubhandarimail@gmail.com>
2026-03-08 12:34:21 -0400

    Implement insertion sort in decreasing order

--- MSCS532_Assignment1/insertion_sort.py
+++ MSCS532_Assignment1/insertion_sort.py
@@ -1,6 +1,22 @@
 # MSCS532 Assignment 1
 # Insertion Sort in monotonically decreasing order
 
+def insertion_sort_desc(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+
+        while j >= 0 and arr[j] < key:
+            arr[j + 1] = arr[j]
+            j -= 1
+
+        arr[j + 1] = key
+
+    return arr
+
+
 numbers = [5, 2, 9, 1, 7, 6]
 
 print("Original array:", numbers)
+sorted_numbers = insertion_sort_desc(numbers)
+print("Sorted array:", sorted_numbers)
